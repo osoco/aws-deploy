0b5c2c557cc934f8234b0b9c2e1f68879e1affe6
Jose San Leandro <jose.sanleandro@osoco.es>
2020-11-11 05:46:13 +0100

    Added support for deleting stacks

--- delete-all.sh
+++ delete-all.sh
@@ -1,31 +1,26 @@
-#!/bin/bash /home/chous/.dry-wit/src/dry-wit
+#!/bin/bash dry-wit
 # Copyright 2018-today Osoco S.L.
 # Distributed under the terms of the GNU General Public License v3
-# mod: deploy-all.sh
+# mod: delete-all.sh
 # api: public
-# txt: Manages the deployment of all stacks as a whole.
+# txt: Manages the deletion of all stacks as a whole.
 
 DW.import aws-deploy;
 
 # fun: main
 # api: public
-# txt: Manages the deployment of all stacks as a whole.
+# txt: Manages the deletion of all stacks as a whole.
 # txt: Returns 0/TRUE always. In case of error, it exits.
 # use: main;
 function main() {
-  deployAll;
+  deleteAll;
 }
 
 # script metadata
-setScriptDescription "Manages the deployment of all stacks as as whole";
+setScriptDescription "Manages the deletion of all stacks as a whole";
 setScriptUrl "https://github.com/osoco/aws-deploy";
 
-addCommandLineParameter action "Either create or update" MANDATORY SINGLE;
 addCommandLineParameter environment "The environment where the stack is or will be deployed. For environment-wide settings, define them in a file [environment].config in the root folder" MANDATORY SINGLE;
 addCommandLineParameter client "The specific client" OPTIONAL SINGLE;
 
-# errors
-addError DEPLOY_STACK_NOT_INSTALLED "deploy-stack.sh is not installed";
-
-checkReq "./deploy-stack.sh" DEPLOY_STACK_NOT_INSTALLED;
 # vim: syntax=sh ts=2 sw=2 sts=4 sr noet
--- delete-stack.sh
+++ delete-stack.sh
@@ -3,23 +3,22 @@
 # Distributed under the terms of the GNU General Public License v3
 # mod: deploy-stack
 # api: public
-# txt: Deploys a Cloudformation stack for given environment/client combination in AWS.
+# txt: Deletes a Cloudformation stack for given environment/client combination in AWS.
 
 DW.import aws-deploy;
 
 # fun: main
 # api: public
-# txt: Deploys a Cloudformation stack for given environment/client combination in AWS.
+# txt: Deletes a Cloudformation stack for given environment/client combination in AWS.
 # txt: Returns 0/TRUE always, but can exit if an error is triggered.
 function main() {
-  createOrUpdateStack "${STACK}" "${ACTION}" "${ENVIRONMENT}" "${CLIENT}";
+  deleteStack "${STACK}" "${ENVIRONMENT}" "${CLIENT}";
 }
 
 # script metadata
-setScriptDescription "Deploys a Cloudformation stack for given environment/client combination in AWS";
+setScriptDescription "Deletes a Cloudformation stack for given environment/client combination in AWS";
 setScriptUrl "https://github.com/osoco/aws-deploy";
 
-addCommandLineParameter action "Either create or update" MANDATORY SINGLE;
 addCommandLineParameter environment "The environment where the stack is or will be deployed. For environment-wide settings, define them in a file [environment].config in the root folder" MANDATORY SINGLE;
 addCommandLineParameter client "The specific client" OPTIONAL SINGLE;
 
